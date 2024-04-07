72c74ef85108f91acf6a529e1f038dc769995fc2
Abhimanyu R B <abhimanyurbsa@gmail.com>
2024-04-07 10:51:03 +0530

    created community app
    fixed migrations

--- admins/models.py
+++ admins/models.py
@@ -1,3 +1,9 @@
+# models.py in the admins app
 from django.db import models
 
-# Create your models here.
+class UserExperience(models.Model):
+    experience = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.experience[:50]  # Display first 50 characters of experience
